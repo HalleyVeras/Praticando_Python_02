55ced7e049625adc77f759cbdd6c6800aaf20957
Halley Veras <136653792+HalleyVeras@users.noreply.github.com>
2023-11-01 00:08:33 -0300

    Add files via upload

---
+++ Aula05_Listas_01.py
@@ -0,0 +1,6 @@
+numbers = [10, 5, 7, 2, 1]
+print("Conteúdos originais da lista:", numbers)  # Imprimindo o conteúdo original da lista.
+
+numbers[0] = 111
+print("Novo conteúdo da lista: ", numbers)  # Conteúdo atual da lista.
+
